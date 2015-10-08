fafb9538f1667d3779ced49c7d61b3de1e0befbd
pj.debarros@gmail.com <pj.debarros@gmail.com>
2015-10-08 00:54:09 -0400

    Changed to a tabsetpanel layout.  Removed the submit button.  Added links to download sample data.  Added the image that explains the relationships between the input files.

--- ui.R
+++ ui.R
@@ -12,21 +12,34 @@ shinyUI(
         textInput("testName", ""),
         selectInput("alphaLevel", 
                     label = "How certain do you want to be about the results?", 
-                    choices = c("90%", "95%", "99%"), selected = "95%"),
-        submitButton("Submit")
-        ),
+                    choices = c("90%", "95%", "99%"), selected = "95%")
+      ),
       
       mainPanel(
-        h3("The process is as follows:"),
-        tags$ol(
-          tags$li("Create CSV (comma separated values) files for last year's predictor exam, last year's regents, and this year's regents."),
-          tags$li("The first column must include student ID numbers, but they should not be actual student ID's.  You can create random numbers to assign to students before creating the files, as long as they are consistent between the old predictor and old regents files."),
-          tags$li("The second column should be the scores.  The values for the regents scores should be between 0 and 100.  The values for the predictor can be on any scale, as long as it is consistent between the two predictor files."),
-          tags$li("Upload each of the files using the buttons on the left.  After you upload the third, your results will appear."),
-          tags$li("To customize the results, enter the name of the exam, select your desired level of certainty, and hit Sumbit"),
-          tags$li("Questions?  Comments?  Want the source code?  Email ", tags$a(href="mailto:pdebarros@greentechhigh.org","pdebarros@greentechhigh.org"),".")
+        tabsetPanel(
+          tabPanel(title = "Instructions",
+                   h3("The process is as follows:"),
+                   tags$ol(
+                     tags$li("Create CSV (comma separated values) files for last year's predictor exam, last year's regents, and this year's regents."),
+                     tags$li("The first column must include student ID numbers, but they should not be actual student ID's.  You can create random numbers to assign to students before creating the files, as long as they are consistent between the old predictor and old regents files."),
+                     tags$li("The second column should be the scores.  The values for the regents scores should be between 0 and 100.  The values for the predictor can be on any scale, as long as it is consistent between the two predictor files."),
+                     tags$li("Upload each of the files using the buttons on the left.  After you upload the third, your results will appear."),
+                     tags$li("To customize the results, enter the name of the exam, select your desired level of certainty, and hit Sumbit"),
+                     tags$li("Questions?  Comments?  Want the source code?  Email ", tags$a(href="mailto:pdebarros@greentechhigh.org","pdebarros@greentechhigh.org"),".")
+                   )
           ),
-        h1(textOutput("response"))
+          tabPanel(title = "Diagram",
+                   tags$img(src = "diagram.jpg")
+          ),
+          tabPanel(title = "Output",
+                   h1(textOutput("response"))
+          ),
+          tabPanel(title = "Sample Data",
+                   downloadLink('Sample1', 'Download Sample Old Predictor Scores'),tags$br(),
+                   downloadLink('Sample2', 'Download Sample New Predictor Scores'),tags$br(),
+                   downloadLink('Sample3', 'Download Sample Regents Scores')
+          )
+        )
       )
     )
   )
