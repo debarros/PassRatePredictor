f5aef713491823ad8c8021d3f60f66890688b299
debarros <pj.debarros@gmail.com>
2017-05-18 14:47:59 -0400

    CleanData.R - added lines to remove empty rows; removed unnecessary 'which' statements

--- CleanData.R
+++ CleanData.R
@@ -10,12 +10,16 @@ CleanData = function(d1, d2, regents){
   colnames(d2) = c("StudNum","Score")
   colnames(regents) = c("StudNum","Score")
   
+  #Remove rows that have no student number
+  d1 = d1[!is.na(d1$StudNum),]
+  d2 = d2[!is.na(d2$StudNum),]
+  regents = regents[!is.na(regents$StudNum),]
   
   #Match the regents scores with the predictor scores
   #This would be better done using the "match()" function, but I didn't know it when I originally wrote this section of code
   for (i in 1:nrow(d1)){                                                     #For each score in the old predictor scores files,
     if (d1$StudNum[i] %in% regents$StudNum){                                 #if this student has an outcome score in the regents file,
-      d1$Regents[i] = regents$Score[which(regents$StudNum == d1$StudNum[i])] #put the outcome score in that row, pairing it with the predictor score.
+      d1$Regents[i] = regents$Score[regents$StudNum == d1$StudNum[i]] #put the outcome score in that row, pairing it with the predictor score.
     }else{                                                                   #If the student does not have an outcome score in the regents file,
       d1$Regents[i] = NA                                                     #put NA in that row to indicate that the student's regents score is missing.
     }
@@ -26,20 +30,20 @@ CleanData = function(d1, d2, regents){
   #Get rid of the missing data rows in the predictor-regents score pairings
   #This section will be eliminated once the procedure for handling missing data is implemented
   #In d1, the "drop" variable indicates whether it should be kept (0) or dropped (>0)
-  d1 = d1[which(d1$drop == 0),]         #Subset d1 to only the rows with complete data
+  d1 = d1[d1$drop == 0,]         #Subset d1 to only the rows with complete data
   
   
   #In case there are extra empty rows at the bottom of the new predictor score file, those should be identified and eliminated
   #This is not an elimination of missing data; it's just cleaning up a messy file
   d2$drop = is.na(d2$StudNum)           #In d2, the drop variable indicates whether the row includes actual data 
-  d2 = d2[which(d2$drop == FALSE),]     #Subset d2 to only those rows that have a student number
+  d2 = d2[d2$drop == FALSE,]     #Subset d2 to only those rows that have a student number
   
   
   #Create the "Passed" variable in d1
   #This is a variable that indicates whether the student passed the regents
   #0 indicates failed, 1 indicates passed
   d1$Passed = 0                           #set the default value to fail
-  d1$Passed[which(d1$Regents >= 65)] = 1  #For the students with regents scores of least 65, set "Passed" to 1 
+  d1$Passed[d1$Regents >= 65] = 1  #For the students with regents scores of least 65, set "Passed" to 1 
   #Note: this will need to be ">= PassCutoff" rather than ">=65", after PassCutoff is defined
   
   return(list(d1, d2))
