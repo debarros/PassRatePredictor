d20888761528a9e7f14e501c6ea4e435bedb5eb5
debarros <pj.debarros@gmail.com>
2017-05-18 14:48:17 -0400

    functions.R - added two more source statements

--- functions.R
+++ functions.R
@@ -7,6 +7,8 @@ source("list.R")            #this contains code that allows functions to return 
 source("CleanData.R")       #function to clean up the data
 source("crossprod2.R")      #function to make matrix multiplication carry over dimension names
 source("BinomArray.R")      #function to use a matrix of proabilities to generate a 3-D array of simulations
+source("OutputSentence.R")  #function to generate the output sentence
+source("StorePredictions.R")
 Sample1 = read.csv("www/SampleOldPredictor.csv")
 Sample2 = read.csv("www/SampleNewPredictor.csv") 
 Sample3 = read.csv("www/SampleRegents.csv")
